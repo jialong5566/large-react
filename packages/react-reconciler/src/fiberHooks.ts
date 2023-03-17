44df24eea0f2d7ea33b8566d556bdbf8d5fc1793
贾龙 <john17@sfmail.sf-express.com>
2023-03-17 10:06:35 +0800

    feat: 14-4 完成任务调度

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,21 +1,21 @@
-import {FiberNode} from './fiber';
+import { FiberNode } from './fiber';
 import internals from 'shared/internals';
-import {Dispatcher} from 'react/src/currentDispatcher';
+import { Dispatcher } from 'react/src/currentDispatcher';
 import {
-  createUpdate,
-  createUpdateQueue,
-  enqueueUpdate,
-  processUpdateQueue,
-  UpdateQueue
+	createUpdate,
+	createUpdateQueue,
+	enqueueUpdate,
+	processUpdateQueue,
+	UpdateQueue
 } from './updateQueue';
-import {Dispatch} from 'react/src/currentDispatcher';
-import {Action} from 'shared/ReactTypes';
-import {scheduleUpdateOnFiber} from './workLoop';
-import {Flags, PassiveEffect} from './fiberFlags';
-import {HookHasEffect, Passive} from './hookEffectTags';
-import {Lane, Lanes, NoLane, NoLanes, requestUpdateLane} from './fiberLanes';
+import { Dispatch } from 'react/src/currentDispatcher';
+import { Action } from 'shared/ReactTypes';
+import { scheduleUpdateOnFiber } from './workLoop';
+import { Flags, PassiveEffect } from './fiberFlags';
+import { HookHasEffect, Passive } from './hookEffectTags';
+import { Lane, Lanes, NoLane, NoLanes, requestUpdateLane } from './fiberLanes';
 
-const {currentDispatcher} = internals;
+const { currentDispatcher } = internals;
 
 let currentlyRenderingFiber: FiberNode | null = null;
 let workInProgressHook: Hook | null = null;
@@ -23,219 +23,219 @@ let currentHook: Hook | null = null;
 let renderLanes: Lanes = NoLanes;
 
 interface Hook {
-  memoizedState: any;
-  updateQueue: unknown;
-  next: Hook | null;
+	memoizedState: any;
+	updateQueue: unknown;
+	next: Hook | null;
 }
 
 export interface Effect {
-  tag: Flags;
-  create: EffectCallback | void;
-  destroy: EffectCallback | void;
-  deps: EffectDeps;
-  next: Effect | null;
+	tag: Flags;
+	create: EffectCallback | void;
+	destroy: EffectCallback | void;
+	deps: EffectDeps;
+	next: Effect | null;
 }
 
 export interface FCUpdateQueue<State> extends UpdateQueue<State> {
-  lastEffect: Effect | null;
+	lastEffect: Effect | null;
 }
 
 type EffectCallback = () => void;
 type EffectDeps = any[] | null;
 
 export function renderWithHooks(wip: FiberNode, lane: Lane) {
-  currentlyRenderingFiber = wip;
-  wip.memoizedState = null;
-  renderLanes = lane;
+	currentlyRenderingFiber = wip;
+	wip.memoizedState = null;
+	renderLanes = lane;
 
-  const current = wip.alternate;
+	const current = wip.alternate;
 
-  if (current !== null) {
-    currentDispatcher.current = HooksDispatcherOnUpdate;
-  } else {
-    currentDispatcher.current = HooksDispatcherOnMount;
-  }
-  const Component = wip.type;
-  const props = wip.pendingProps;
-  const children = Component(props);
+	if (current !== null) {
+		currentDispatcher.current = HooksDispatcherOnUpdate;
+	} else {
+		currentDispatcher.current = HooksDispatcherOnMount;
+	}
+	const Component = wip.type;
+	const props = wip.pendingProps;
+	const children = Component(props);
 
-  currentlyRenderingFiber = null;
-  workInProgressHook = null;
-  currentHook = null;
-  renderLanes = NoLane;
-  return children;
+	currentlyRenderingFiber = null;
+	workInProgressHook = null;
+	currentHook = null;
+	renderLanes = NoLane;
+	return children;
 }
 
 const HooksDispatcherOnMount: Dispatcher = {
-  useState: mountState,
-  useEffect: updateEffect //todo
+	useState: mountState,
+	useEffect: updateEffect //todo
 };
 const HooksDispatcherOnUpdate: Dispatcher = {
-  useState: updateState,
-  useEffect: updateEffect
+	useState: updateState,
+	useEffect: updateEffect
 };
 
 function mountEffect(create: EffectCallback | void, deps: EffectDeps) {
-  const hook = mountWorkInProgressHook();
-  const nextDeps = deps === undefined ? null : deps;
-  (currentlyRenderingFiber as FiberNode).flags |= PassiveEffect;
-  hook.memoizedState = pushEffect(
-      Passive | HookHasEffect,
-      create,
-      undefined,
-      nextDeps
-  );
+	const hook = mountWorkInProgressHook();
+	const nextDeps = deps === undefined ? null : deps;
+	(currentlyRenderingFiber as FiberNode).flags |= PassiveEffect;
+	hook.memoizedState = pushEffect(
+		Passive | HookHasEffect,
+		create,
+		undefined,
+		nextDeps
+	);
 }
 
 function pushEffect(
-    hookFlags: Flags,
-    create: EffectCallback | void,
-    destroy: EffectCallback | void,
-    deps: EffectDeps
+	hookFlags: Flags,
+	create: EffectCallback | void,
+	destroy: EffectCallback | void,
+	deps: EffectDeps
 ): Effect {
-  const effect: Effect = {
-    tag: hookFlags,
-    create,
-    destroy,
-    deps,
-    next: null
-  };
+	const effect: Effect = {
+		tag: hookFlags,
+		create,
+		destroy,
+		deps,
+		next: null
+	};
 
-  const fiber = currentlyRenderingFiber as FiberNode;
-  const updateQueue = fiber.updateQueue as FCUpdateQueue<any>;
-  if (updateQueue === null) {
-    const updateQueue = createFCUpdateQueue();
-    fiber.updateQueue = updateQueue;
-    effect.next = effect;
-    updateQueue.lastEffect = effect;
-  } else {
-    const lastEffect = updateQueue.lastEffect;
-    if (lastEffect === null) {
-      effect.next = effect;
-      updateQueue.lastEffect = effect;
-    } else {
-      const firstEffect = lastEffect.next;
-      lastEffect.next = effect;
-      effect.next = firstEffect;
-      updateQueue.lastEffect = effect;
-    }
-  }
-  return effect;
+	const fiber = currentlyRenderingFiber as FiberNode;
+	const updateQueue = fiber.updateQueue as FCUpdateQueue<any>;
+	if (updateQueue === null) {
+		const updateQueue = createFCUpdateQueue();
+		fiber.updateQueue = updateQueue;
+		effect.next = effect;
+		updateQueue.lastEffect = effect;
+	} else {
+		const lastEffect = updateQueue.lastEffect;
+		if (lastEffect === null) {
+			effect.next = effect;
+			updateQueue.lastEffect = effect;
+		} else {
+			const firstEffect = lastEffect.next;
+			lastEffect.next = effect;
+			effect.next = firstEffect;
+			updateQueue.lastEffect = effect;
+		}
+	}
+	return effect;
 }
 
 function createFCUpdateQueue<State>() {
-  const updateQueue = createUpdateQueue<State>() as FCUpdateQueue<State>;
-  updateQueue.lastEffect = null;
-  return updateQueue;
+	const updateQueue = createUpdateQueue<State>() as FCUpdateQueue<State>;
+	updateQueue.lastEffect = null;
+	return updateQueue;
 }
 
 function updateEffect() {
-  console.warn("updateEffect");
+	console.warn('updateEffect');
 }
 
 function updateState<State>(): [State, Dispatch<State>] {
-  const hook = updateWorkInProgressHook();
+	const hook = updateWorkInProgressHook();
 
-  const queue = hook.updateQueue as UpdateQueue<State>;
-  const pending = queue.shared.pending;
-  queue.shared.pending = null;
-  if (pending !== null) {
-    const {memoizedState} = processUpdateQueue(
-        hook.memoizedState,
-        pending,
-        renderLanes
-    );
-    hook.memoizedState = memoizedState;
-  }
-  return [hook.memoizedState, queue.dispatch as Dispatch<State>];
+	const queue = hook.updateQueue as UpdateQueue<State>;
+	const pending = queue.shared.pending;
+	queue.shared.pending = null;
+	if (pending !== null) {
+		const { memoizedState } = processUpdateQueue(
+			hook.memoizedState,
+			pending,
+			renderLanes
+		);
+		hook.memoizedState = memoizedState;
+	}
+	return [hook.memoizedState, queue.dispatch as Dispatch<State>];
 }
 
 function mountState<State>(
-    initialState: (() => State) | State
+	initialState: (() => State) | State
 ): [State, Dispatch<State>] {
-  const hook = mountWorkInProgressHook();
-  let memoizedState;
-  if (initialState instanceof Function) {
-    memoizedState = initialState();
-  } else {
-    memoizedState = initialState;
-  }
+	const hook = mountWorkInProgressHook();
+	let memoizedState;
+	if (initialState instanceof Function) {
+		memoizedState = initialState();
+	} else {
+		memoizedState = initialState;
+	}
 
-  const queue = createUpdateQueue<State>();
-  hook.updateQueue = queue;
-  hook.memoizedState = memoizedState;
+	const queue = createUpdateQueue<State>();
+	hook.updateQueue = queue;
+	hook.memoizedState = memoizedState;
 
-  // @ts-ignore
-  const dispatch = dispatchSetState.bind(null, currentlyRenderingFiber, queue);
-  queue.dispatch = dispatch;
-  return [memoizedState, dispatch];
+	// @ts-ignore
+	const dispatch = dispatchSetState.bind(null, currentlyRenderingFiber, queue);
+	queue.dispatch = dispatch;
+	return [memoizedState, dispatch];
 }
 
 function dispatchSetState<State>(
-    fiber: FiberNode,
-    updateQueue: UpdateQueue<State>,
-    action: Action<State>
+	fiber: FiberNode,
+	updateQueue: UpdateQueue<State>,
+	action: Action<State>
 ) {
-  const lane = requestUpdateLane();
-  const update = createUpdate(action, lane);
-  enqueueUpdate(updateQueue, update);
-  scheduleUpdateOnFiber(fiber, lane);
+	const lane = requestUpdateLane();
+	const update = createUpdate(action, lane);
+	enqueueUpdate(updateQueue, update);
+	scheduleUpdateOnFiber(fiber, lane);
 }
 
 function mountWorkInProgressHook(): Hook {
-  const hook: Hook = {
-    memoizedState: null,
-    updateQueue: null,
-    next: null
-  };
+	const hook: Hook = {
+		memoizedState: null,
+		updateQueue: null,
+		next: null
+	};
 
-  if (workInProgressHook === null) {
-    if (currentlyRenderingFiber === null) {
-      throw new Error('请在函数组件内调用 hook');
-    } else {
-      workInProgressHook = hook;
-      currentlyRenderingFiber.memoizedState = workInProgressHook;
-    }
-  } else {
-    workInProgressHook.next = hook;
-    workInProgressHook = hook;
-  }
-  return workInProgressHook;
+	if (workInProgressHook === null) {
+		if (currentlyRenderingFiber === null) {
+			throw new Error('请在函数组件内调用 hook');
+		} else {
+			workInProgressHook = hook;
+			currentlyRenderingFiber.memoizedState = workInProgressHook;
+		}
+	} else {
+		workInProgressHook.next = hook;
+		workInProgressHook = hook;
+	}
+	return workInProgressHook;
 }
 
 function updateWorkInProgressHook(): Hook {
-  let nextCurrentHook: Hook | null = null;
-  if (currentHook === null) {
-    const current = currentlyRenderingFiber?.alternate;
-    if (current !== null) {
-      nextCurrentHook = current?.memoizedState;
-    } else {
-      nextCurrentHook = null;
-    }
-  } else {
-    nextCurrentHook = currentHook.next;
-  }
-  if (nextCurrentHook === null) {
-    throw new Error('组件本次执行的hook 和 上次的数量不一样');
-  }
+	let nextCurrentHook: Hook | null = null;
+	if (currentHook === null) {
+		const current = currentlyRenderingFiber?.alternate;
+		if (current !== null) {
+			nextCurrentHook = current?.memoizedState;
+		} else {
+			nextCurrentHook = null;
+		}
+	} else {
+		nextCurrentHook = currentHook.next;
+	}
+	if (nextCurrentHook === null) {
+		throw new Error('组件本次执行的hook 和 上次的数量不一样');
+	}
 
-  currentHook = nextCurrentHook;
-  const newHook: Hook = {
-    memoizedState: currentHook?.memoizedState,
-    updateQueue: currentHook?.updateQueue,
-    next: null
-  };
+	currentHook = nextCurrentHook;
+	const newHook: Hook = {
+		memoizedState: currentHook?.memoizedState,
+		updateQueue: currentHook?.updateQueue,
+		next: null
+	};
 
-  if (workInProgressHook === null) {
-    if (currentlyRenderingFiber === null) {
-      throw new Error('请在函数组件内调用 hook');
-    } else {
-      workInProgressHook = newHook;
-      currentlyRenderingFiber.memoizedState = workInProgressHook;
-    }
-  } else {
-    workInProgressHook.next = newHook;
-    workInProgressHook = newHook;
-  }
-  return workInProgressHook;
+	if (workInProgressHook === null) {
+		if (currentlyRenderingFiber === null) {
+			throw new Error('请在函数组件内调用 hook');
+		} else {
+			workInProgressHook = newHook;
+			currentlyRenderingFiber.memoizedState = workInProgressHook;
+		}
+	} else {
+		workInProgressHook.next = newHook;
+		workInProgressHook = newHook;
+	}
+	return workInProgressHook;
 }
--- packages/react-reconciler/src/workLoop.ts
+++ packages/react-reconciler/src/workLoop.ts
@@ -1,168 +1,168 @@
-import {createWorkInProgress, FiberNode, FiberRootNode} from './fiber';
-import {beginWork} from './beginWork';
-import {completeWork} from './completeWork';
-import {HostRoot} from './workTags';
-import {MutationMask, NoFlags} from './fiberFlags';
-import {commitMutationEffects} from './commitWork';
+import { createWorkInProgress, FiberNode, FiberRootNode } from './fiber';
+import { beginWork } from './beginWork';
+import { completeWork } from './completeWork';
+import { HostRoot } from './workTags';
+import { MutationMask, NoFlags } from './fiberFlags';
+import { commitMutationEffects } from './commitWork';
 import {
-  getHighestPriorityLane,
-  Lane,
-  Lanes,
-  markRootFinished,
-  mergeLanes,
-  NoLane,
-  NoLanes,
-  SyncLane
+	getHighestPriorityLane,
+	Lane,
+	Lanes,
+	markRootFinished,
+	mergeLanes,
+	NoLane,
+	NoLanes,
+	SyncLane
 } from './fiberLanes';
-import {flushSyncCallbacks, scheduleSyncCallback} from './syncTaskQueue';
-import {scheduleMicrotask} from 'hostConfig';
+import { flushSyncCallbacks, scheduleSyncCallback } from './syncTaskQueue';
+import { scheduleMicrotask } from 'hostConfig';
 
 let workInProgress: FiberNode | null = null;
 let workInProgressRootRenderLane: Lanes = NoLanes;
 
 function prepareFreshStack(root: FiberRootNode, lane: Lane) {
-  workInProgress = createWorkInProgress(root.current, {});
-  workInProgressRootRenderLane = lane;
+	workInProgress = createWorkInProgress(root.current, {});
+	workInProgressRootRenderLane = lane;
 }
 
 export function scheduleUpdateOnFiber(fiber: FiberNode, lane: Lane) {
-  const root = markUpdateFromFiberToRoot(fiber);
-  markRootUpdated(root, lane);
+	const root = markUpdateFromFiberToRoot(fiber);
+	markRootUpdated(root, lane);
 
-  ensureRootIsScheduled(root);
+	ensureRootIsScheduled(root);
 }
 
 function ensureRootIsScheduled(root: FiberRootNode) {
-  const updateLane = getHighestPriorityLane(root.pendingLanes);
+	const updateLane = getHighestPriorityLane(root.pendingLanes);
 
-  if (updateLane === NoLane) {
-    return;
-  }
+	if (updateLane === NoLane) {
+		return;
+	}
 
-  if (updateLane === SyncLane) {
-    if (__DEV__) {
-      console.log('在微任务中调度，优先级', updateLane);
-    }
-    scheduleSyncCallback(performSyncWorkOnRoot.bind(null, root, updateLane));
-    scheduleMicrotask(flushSyncCallbacks);
-  } else {
-    if (__DEV__) {
-      console.log('在宏任务中调度，优先级', updateLane);
-    }
-  }
+	if (updateLane === SyncLane) {
+		if (__DEV__) {
+			console.log('在微任务中调度，优先级', updateLane);
+		}
+		scheduleSyncCallback(performSyncWorkOnRoot.bind(null, root, updateLane));
+		scheduleMicrotask(flushSyncCallbacks);
+	} else {
+		if (__DEV__) {
+			console.log('在宏任务中调度，优先级', updateLane);
+		}
+	}
 }
 
 function markRootUpdated(root: FiberRootNode, lane: Lane) {
-  root.pendingLanes = mergeLanes(root.pendingLanes, lane);
+	root.pendingLanes = mergeLanes(root.pendingLanes, lane);
 }
 
 function markUpdateFromFiberToRoot(fiber: FiberNode) {
-  let node = fiber;
-  let parent = node.return;
-  while (parent !== null) {
-    node = parent;
-    parent = node.return;
-  }
+	let node = fiber;
+	let parent = node.return;
+	while (parent !== null) {
+		node = parent;
+		parent = node.return;
+	}
 
-  if (node.tag === HostRoot) {
-    return node.stateNode;
-  }
-  return null;
+	if (node.tag === HostRoot) {
+		return node.stateNode;
+	}
+	return null;
 }
 
 function performSyncWorkOnRoot(root: FiberRootNode, lane: Lane) {
-  const nextLanes = getHighestPriorityLane(root.pendingLanes);
-  if (nextLanes !== SyncLane) {
-    ensureRootIsScheduled(root);
-    return;
-  }
-  prepareFreshStack(root, lane);
+	const nextLanes = getHighestPriorityLane(root.pendingLanes);
+	if (nextLanes !== SyncLane) {
+		ensureRootIsScheduled(root);
+		return;
+	}
+	prepareFreshStack(root, lane);
 
-  do {
-    try {
-      workLoop();
-      break;
-    } catch (e) {
-      if (__DEV__) {
-        console.warn('workLoop call error');
-      }
-    }
-  } while (true);
+	do {
+		try {
+			workLoop();
+			break;
+		} catch (e) {
+			if (__DEV__) {
+				console.warn('workLoop call error');
+			}
+		}
+	} while (true);
 
-  const finishedWork = root.current.alternate;
-  root.finishedWork = finishedWork;
-  root.finishedLane = lane;
+	const finishedWork = root.current.alternate;
+	root.finishedWork = finishedWork;
+	root.finishedLane = lane;
 
-  workInProgressRootRenderLane = NoLane;
-  commitRoot(root);
+	workInProgressRootRenderLane = NoLane;
+	commitRoot(root);
 }
 
 function commitRoot(root: FiberRootNode) {
-  const finishedWork = root.finishedWork;
+	const finishedWork = root.finishedWork;
 
-  if (finishedWork === null) {
-    return;
-  }
-  if (__DEV__) {
-    console.warn('commit begin !!');
-  }
+	if (finishedWork === null) {
+		return;
+	}
+	if (__DEV__) {
+		console.warn('commit begin !!');
+	}
 
-  const lane = root.finishedLane;
+	const lane = root.finishedLane;
 
-  if (lane === NoLane && __DEV__) {
-    console.error('commitRoot: root.finishedLane should not be NoLane');
-  }
-  root.finishedWork = null;
-  root.finishedLane = NoLane;
-  markRootFinished(root, lane);
+	if (lane === NoLane && __DEV__) {
+		console.error('commitRoot: root.finishedLane should not be NoLane');
+	}
+	root.finishedWork = null;
+	root.finishedLane = NoLane;
+	markRootFinished(root, lane);
 
-  const subtreeHasEffect =
-      (finishedWork.subtreeFlags & MutationMask) !== NoFlags;
+	const subtreeHasEffect =
+		(finishedWork.subtreeFlags & MutationMask) !== NoFlags;
 
-  const rootHasEffect = (finishedWork.flags & MutationMask) !== NoFlags;
+	const rootHasEffect = (finishedWork.flags & MutationMask) !== NoFlags;
 
-  if (subtreeHasEffect || rootHasEffect) {
-    // mutation
-    commitMutationEffects(finishedWork);
-    root.current = finishedWork;
-    // layout
-  } else {
-    // Fiber Tree切换
-    root.current = finishedWork;
-  }
+	if (subtreeHasEffect || rootHasEffect) {
+		// mutation
+		commitMutationEffects(finishedWork);
+		root.current = finishedWork;
+		// layout
+	} else {
+		// Fiber Tree切换
+		root.current = finishedWork;
+	}
 }
 
 function workLoop() {
-  while (workInProgress !== null) {
-    performUnitOfWork(workInProgress, workInProgressRootRenderLane);
-  }
+	while (workInProgress !== null) {
+		performUnitOfWork(workInProgress, workInProgressRootRenderLane);
+	}
 }
 
 function performUnitOfWork(fiber: FiberNode, renderLanes: Lanes) {
-  const next = beginWork(fiber, renderLanes);
-  fiber.memoizedProps = fiber.pendingProps;
+	const next = beginWork(fiber, renderLanes);
+	fiber.memoizedProps = fiber.pendingProps;
 
-  if (next === null) {
-    completeUnitOfWork(fiber);
-  } else {
-    workInProgress = next;
-  }
+	if (next === null) {
+		completeUnitOfWork(fiber);
+	} else {
+		workInProgress = next;
+	}
 }
 
 function completeUnitOfWork(fiber: FiberNode) {
-  let node: FiberNode | null = fiber;
-  do {
-    const next = completeWork(node);
-    if (next !== null) {
-      workInProgress = next;
-      return;
-    }
-    const sibling = node.sibling;
-    if (sibling !== null) {
-      workInProgress = sibling;
-      return;
-    }
-    node = node.return;
-    workInProgress = node;
-  } while (node !== null);
+	let node: FiberNode | null = fiber;
+	do {
+		const next = completeWork(node);
+		if (next !== null) {
+			workInProgress = next;
+			return;
+		}
+		const sibling = node.sibling;
+		if (sibling !== null) {
+			workInProgress = sibling;
+			return;
+		}
+		node = node.return;
+		workInProgress = node;
+	} while (node !== null);
 }
